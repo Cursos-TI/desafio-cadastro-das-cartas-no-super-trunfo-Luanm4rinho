6c6d2c2c99d2129cbc90a6b572d9ff94673f2db5
Luanm4rinho <luanmarinho.est@gmail.com>
2025-03-07 01:15:40 +0000

    Desafionovato

---
+++ SupertrunfoNovato.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main(){
+    printf("Carta 1: \n");
+    char estado;
+    char codigo[03];
+    char nome[20];
+    int populacao;
+    float
+
+    printf("Digite o estado: \n");
+    scanf("%c", &estado);
+    printf("Digite o código da carta: \n");
+    scanf("%s", &codigo);
+    printf("Digite o nome das cidade: \n");
+    scanf("%s", &nome);
+    printf("Infome a população: \n");
+    scanf("%d", &populacao);
+}
